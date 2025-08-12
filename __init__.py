c8070db60f9b0b22ae8c9c0dbe883dc4f5d0e0eb
MATEUSZxcv <mateusz.kuc@outlook.com>
2025-08-12 13:29:06 +0200

    create __init__.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+ #TODO: License
+
+bl_info = {
+    "name": "Blender YOLO Autolabel",
+    "author": "Mateusz Kuc",
+    "description": "Automatically generates YOLO-compatible labels for objects in Blender.",
+    "version": (1, 0, 0),
+    "blender": (2, 80, 0),
+    "location": "View3D > Sidebar > YOLO Autolabel",
+    "url": "https://github.com/qm4teo/Blender-YOLO-Autolabel",
+    "wiki_url": "https://github.com/qm4teo/Blender-YOLO-Autolabel",
+    "tracker_url": "https://github.com/qm4teo/Blender-YOLO-Autolabel/issues",
+    "category": "Objects"
+}
+
