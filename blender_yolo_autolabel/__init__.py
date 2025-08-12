773d4e7c3c3c76b1fd11160fa7451bb32e4f8514
MATEUSZxcv <mateusz.kuc@outlook.com>
2025-08-12 19:40:44 +0200

    update minimal blender version

--- blender_yolo_autolabel/__init__.py
+++ blender_yolo_autolabel/__init__.py
@@ -19,7 +19,7 @@ bl_info = {
     "author": "Mateusz Kuc",
     "description": "Automatically generates YOLO-compatible labels for objects in Blender.",
     "version": (1, 0, 0),
-    "blender": (2, 80, 0),
+    "blender": (4, 1, 0),
     "location": "View3D > Sidebar > YOLO Autolabel",
     "url": "https://github.com/qm4teo/Blender-YOLO-Autolabel",
     "wiki_url": "https://github.com/qm4teo/Blender-YOLO-Autolabel",
